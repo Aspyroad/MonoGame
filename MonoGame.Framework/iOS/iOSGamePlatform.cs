0943902b4198d727cf47e9c0b00651439cb7a0f4
Brett Anthony <brettanthony@outlook.com>
2018-09-20 09:41:25 +1000

    Remove MakeKey and Rootview Controller from iOSGamePlatform.

--- MonoGame.Framework/iOS/iOSGamePlatform.cs
+++ MonoGame.Framework/iOS/iOSGamePlatform.cs
@@ -212,12 +212,12 @@ namespace Microsoft.Xna.Framework
         public override void StartRunLoop()
         {
             // Show the window
-            _mainWindow.MakeKeyAndVisible();
+            //_mainWindow.MakeKeyAndVisible();
 
             // In iOS 8+ we need to set the root view controller *after* Window MakeKey
             // This ensures that the viewController's supported interface orientations
             // will be respected at launch
-            _mainWindow.RootViewController = _viewController;
+           // _mainWindow.RootViewController = _viewController;
 
             BeginObservingUIApplication();
 
